087bf173e098bb4c0a0e8599a4b7b7766287e99a
noracurcio <noracurcio@gmail.com>
2020-11-03 14:27:59 -0700

    changes to styling and layout.

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 ### Schema
 
-CREATE DATABASE burger_db;
-USE burger_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
