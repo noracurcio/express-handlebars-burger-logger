1eb0e7ab76a0d4d98ecd12013272c37ede4651f0
noracurcio <noracurcio@gmail.com>
2020-11-03 13:52:42 -0700

    Initial commit.

---
+++ node-express-burger-logger/db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ;
+INSERT INTO burgers (burger_name) VALUES ;
+INSERT INTO burgers (burger_name, devoured) VALUES;
+INSERT INTO burgers (burger_name, devoured) VALUES ;
+INSERT INTO burgers (burger_name, devoured) VALUES ;
+INSERT INTO burgers (burger_name) VALUES ;
