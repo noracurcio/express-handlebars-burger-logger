8c8ac4b32446be256df6703dc74b4ea3cf6088c5
noracurcio <noracurcio@gmail.com>
2020-11-03 21:04:27 -0700

    ugh.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Icuryy4me!",
-  database: "burger_db"
-});
+if (proccess.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+} else  {
+    connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Icuryy4me!',
+    database: 'burger_db'
+
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
